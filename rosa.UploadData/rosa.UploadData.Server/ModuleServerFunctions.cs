01a5b023f30829ddfc400030d23e5b6eda363f44
Samarin_VV <Samarin_VV@directum.ru>
2023-06-14 10:06:04 +0400

    Доработки после рецензирования

--- rosa.UploadData/rosa.UploadData.Server/ModuleServerFunctions.cs
+++ rosa.UploadData/rosa.UploadData.Server/ModuleServerFunctions.cs
@@ -1138,8 +1138,8 @@ namespace rosa.UploadData.Server
     [Public(WebApiRequestType = RequestType.Post)]
     public static bool AddMunicipalHierarchyCity(string objectId, string parentObjId)
     {
-      long id = 0;
-      long parentId = 0;
+      var id = 0L;
+      var parentId = 0L;
       if (!long.TryParse(objectId, out id) || !long.TryParse(parentObjId, out parentId))
         return false;
       var city = GD.GovernmentSolution.Cities.GetAll(c => c.FiasInnerIdGD == objectId).FirstOrDefault();
@@ -1172,7 +1172,8 @@ namespace rosa.UploadData.Server
     [Public(WebApiRequestType = RequestType.Post)]
     public static bool AddMunicipalHierarchySettlement(string objectId, string parentObjId)
     {
-      long id = 0, parentId = 0;
+      var id = 0L;
+      var parentId = 0L;
       if (!long.TryParse(objectId, out id) || !long.TryParse(parentObjId, out parentId))
         return false;
       var settlement = GD.GovernmentCommons.Settlements.GetAll(m => m.FiasInnerId == objectId).FirstOrDefault();
@@ -1201,7 +1202,7 @@ namespace rosa.UploadData.Server
     [Public(WebApiRequestType = RequestType.Get)]
     public static List<string> GetCityIds(string regionCode)
     {
-      var region = Sungero.Commons.Regions.GetAll(r => r.Code!= null && r.Code.ToUpper() == regionCode.ToUpper()).FirstOrDefault();
+      var region = Sungero.Commons.Regions.GetAll(r => r.Code != null && r.Code.ToUpper() == regionCode.ToUpper()).FirstOrDefault();
       if (region != null)
         return GD.GovernmentSolution.Cities.GetAll(c => c.FiasInnerIdGD != null && Equals(c.Region, region)).Select(c => c.FiasInnerIdGD).ToList();
       else
